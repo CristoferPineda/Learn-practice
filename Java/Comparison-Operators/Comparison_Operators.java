d136ad97c75a89ab268528d35c07dce7616327ca
Cristofer Pineda <cristofer.pineda.jauregui@gmail.com>
2025-11-08 00:33:59 -0600

    Add Comparison Operators practice

---
+++ Java/Comparison-Operators/Comparison_Operators.java
@@ -0,0 +1,15 @@
+
+
+public class Comparison_Operators {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        System.out.println("a == b = " + (a == b) ); //False
+        System.out.println("a != b = " + (a != b) ); //True
+        System.out.println("a > b = " + (a > b) ); //False
+        System.out.println("a < b = " + (a < b) ); //True
+        System.out.println("b >= a = " + (b >= a) ); //True
+        System.out.println("b <= a = " + (b <= a) ); //False
+    }
+}
+
